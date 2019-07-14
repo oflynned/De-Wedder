8df36af4f1f991878cb0a824087d382848a835e5
Edmond O'Flynn <oflynned@tcd.ie>
2019-07-14 14:32:19 +0200

    add component map

--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -1,14 +1,14 @@
 import React, {Component} from "react";
 
-import {fetchWeatherAPI} from "../api/api";
-
 import "./weather.css";
 
 import Rain from "./forecast/rain";
 import Cloud from "./forecast/cloud";
-
-// TODO ask for location permissions
-const LONGITUDE = 4.34878, LATITUDE = 50.85045;
+import Clear from "./forecast/clear";
+import Snow from "./forecast/snow";
+import Thunderstorm from "./forecast/thunderstorm";
+import Drizzle from "./forecast/drizzle";
+import {fetchWeatherAPI} from "../api/api";
 
 class Weather extends Component {
     constructor() {
@@ -24,16 +24,20 @@ class Weather extends Component {
     }
 
     componentDidMount() {
-        fetchWeatherAPI(LONGITUDE, LATITUDE)
-            .then(({weather, main}) => {
+        return new Promise(res => navigator.geolocation.getCurrentPosition(({coords}) => res(coords)))
+            .then(({latitude, longitude}) => {
+                return fetchWeatherAPI(latitude, longitude);
+            })
+            .then(({weather, main, name}) => {
                 const {temp: temperature, humidity} = main;
                 const {main: conditions, description, id: iconId} = weather[0];
                 this.setState({
                     conditions,
                     description,
-                    temperature: Math.round(temperature - 273.15),
+                    temperature: Math.round(temperature),
                     humidity,
-                    iconId
+                    iconId,
+                    city: Weather.formatCity(name)
                 });
             })
             .catch(err => {
@@ -42,37 +46,79 @@ class Weather extends Component {
     }
 
     render() {
-        let currentWeather = this.assignWeather();
+        let {component, gradient} = this.assignWeather();
         return (
-            <div className={"weather gradient-cloudy"}>
-                <div className={"container"}>
-                    {currentWeather}
+            <div className={"weather"}>
+                <div className={`container ${gradient}`}>
+                    {component}
                 </div>
             </div>
         );
     }
 
+    static formatCity(city) {
+        return city.replace("(", "").replace(")", "");
+    }
+
     assignWeather() {
         switch (this.state.conditions) {
-            case "Thunderstorm":
             case "Drizzle":
             case "Mist":
             case "Smoke":
             case "Haze":
             case "Dust":
             case "Fog":
-            case "Clear":
-            case "Thunder":
             case "Ash":
+                return {
+                    component: <Drizzle temperature={this.state.temperature}
+                                        description={this.state.description}
+                                        iconId={this.state.iconId}
+                                        city={this.state.city}/>,
+                    gradient: "gradient-rain"
+                };
             case "Squall":
-            case "Tornado":
-            case "Clouds":
-                return <Cloud
-                    temperature={this.state.temperature}
-                    description={this.state.description}
-                    iconId={this.state.iconId}/>;
             case "Rain":
-                return <Rain description={this.state.description} iconId={this.state.iconId}/>;
+                return {
+                    component: <Rain temperature={this.state.temperature}
+                                     description={this.state.description}
+                                     iconId={this.state.iconId}
+                                     city={this.state.city}/>,
+                    gradient: "gradient-rain"
+                };
+            case "Tornado":
+            case "Thunder":
+            case "Thunderstorm":
+                return {
+                    component: <Thunderstorm temperature={this.state.temperature}
+                                             description={this.state.description}
+                                             iconId={this.state.iconId}
+                                             city={this.state.city}/>,
+                    gradient: "gradient-thunderstorm"
+                };
+            case "Snow":
+                return {
+                    component: <Snow temperature={this.state.temperature}
+                                     description={this.state.description}
+                                     iconId={this.state.iconId}
+                                     city={this.state.city}/>,
+                    gradient: "gradient-snow"
+                };
+            case "Clear":
+                return {
+                    component: <Clear temperature={this.state.temperature}
+                                      description={this.state.description}
+                                      iconId={this.state.iconId}
+                                      city={this.state.city}/>,
+                    gradient: "gradient-clear"
+                };
+            case "Clouds":
+                return {
+                    component: <Cloud temperature={this.state.temperature}
+                                      description={this.state.description}
+                                      iconId={this.state.iconId}
+                                      city={this.state.city}/>,
+                    gradient: "gradient-cloudy"
+                };
             default:
                 return this.state.loading ? <div>Loading...</div> : <div>Unknown weather type!</div>;
         }
