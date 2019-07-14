e9426c2ebff1d19a8fe860b92e95df1dd4960f1e
Edmond O'Flynn <oflynned@tcd.ie>
2019-07-14 14:32:48 +0200

    add drizzle component

--- src/components/forecast/drizzle.jsx
+++ src/components/forecast/drizzle.jsx
@@ -1,16 +1,16 @@
 import React, {Component} from "react";
 import WeatherIcon from 'react-icons-weather';
 
-class Thunderstorm extends Component {
+class Drizzle extends Component {
     render() {
         return (
             <div>
                 <WeatherIcon name="owm" iconId={this.props.iconId}/>
-                <h1>It's {this.props.temperature}&deg;C in Brussels</h1>
-                <h3>"It's spilling hey"</h3>
+                <h1>It's {this.props.temperature}&deg;C in {this.props.city}</h1>
+                <h3>"It's pure soft hey"</h3>
             </div>
         );
     }
 }
 
-export default Thunderstorm;
+export default Drizzle;
