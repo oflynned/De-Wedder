a68f9c7ce34564842e5f090cd1da74f048d93176
Edmond O'Flynn <oflynned@tcd.ie>
2019-07-14 14:32:55 +0200

    add rain component

--- src/components/forecast/rain.jsx
+++ src/components/forecast/rain.jsx
@@ -1,10 +1,13 @@
 import React, {Component} from "react";
+import WeatherIcon from "react-icons-weather";
 
 class Rain extends Component {
     render() {
         return (
             <div>
-                It's raining
+                <WeatherIcon name="owm" iconId={this.props.iconId}/>
+                <h1>It's {this.props.temperature}&deg;C in {this.props.city}</h1>
+                <h3>"It's lashing hey"</h3>
             </div>
         )
     }
